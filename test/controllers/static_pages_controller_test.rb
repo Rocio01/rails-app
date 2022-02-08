a9a978355a17c0ef8fc07bdbf18d2ae2b803168e
Zulma <zrmartinezg@gmail.com>
2022-02-08 14:17:22 -0500

    finish static pages

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,19 +1,28 @@
 require "test_helper"
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
+  test "should get root" do
+    get root_url
+    assert_response :success
+    assert_select "title", "Home"
+  end
+
   test "should get home" do
     get static_pages_home_url
     assert_response :success
+    assert_select "title", "Home"
   end
 
   test "should get help" do
     get static_pages_help_url
     assert_response :success
+    assert_select "title", "Help"
   end
 
   test "should get about" do
     get static_pages_about_url
     assert_response :success
+    assert_select "title", "About"
   end
 
 end
